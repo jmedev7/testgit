bc76207ccac82ef2867438f156cf627035a0164b
mathewjoy <mathewjoy@hotmail.com>
2024-12-04 22:14:33 -0500

    many updates

---
+++ testgit/this_test.py
@@ -0,0 +1,8 @@
+import logging
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    #feature/add_log mathewjoy 120524 to add necessary loging
+    logging.info("This is an example of logging")
+    print("Hellow world")
+    logging.info("Printed hello world")
